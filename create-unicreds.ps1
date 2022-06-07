6d86aa62dd814567df6c65e691225b7decf0f2cb
ksnook <60659123+ksnook@users.noreply.github.com>
2022-06-07 15:22:40 +0100

    Add files via upload

---
+++ create-unicreds.ps1
@@ -0,0 +1,23 @@
+﻿$creds = Get-Credential
+
+# Here, we'll randomly generate a 32-byte encryption key.  You would load this up from secure storage somewhere, ideally.
+
+$key = New-Object byte[](32)
+
+$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
+$rng.GetBytes($key)
+
+$exportObject = New-Object psobject -Property @{
+    UserName = $creds.UserName
+    Password = ConvertFrom-SecureString -SecureString $creds.Password -Key $key
+}
+
+$exportObject | Export-Clixml -Path .\savedCreds.xml
+
+# To read these back in later, reverse this process (using the same key):
+
+$importObject = Import-Clixml -Path .\savedCreds.xml
+
+$secureString = ConvertTo-SecureString -String $importObject.Password -Key $key
+
+$savedCreds = New-Object System.Management.Automation.PSCredential($importObject.UserName, $secureString)
